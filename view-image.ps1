500d2e335565f438580274cb09d5c39d9ae5ef9d
MohamedAbderraouf <77074550+MohamedAbderraouf@users.noreply.github.com>
2025-03-13 13:27:19 +0100

    Update view-image.ps1

--- view-image.ps1
+++ view-image.ps1
@@ -1,1 +1,6 @@
+# PowerShell script to create a file and write "Hello" inside
+$filepath = "C:\Users\Public\test_output.txt"
+"Hello" | Out-File -FilePath $filepath -Encoding UTF8
 
+# Display a confirmation message
+Write-Host "Script executed! File created at: $filepath"
